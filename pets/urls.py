b53154a8b94864e137328f17bb44037d08f0e8ec
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2025-09-01 17:07:29 +0300

    URL config

--- pets/urls.py
+++ pets/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PetViewSet, AdoptionRequestViewSet, SignupView
+
+router = DefaultRouter()
+router.register(r'pets', PetViewSet, basename='pet')
+router.register(r'adoption-requests', AdoptionRequestViewSet, basename='adoptionrequest')
 
 urlpatterns = [
-    
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('', include(router.urls)),
 ]

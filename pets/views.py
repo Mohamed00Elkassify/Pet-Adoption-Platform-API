00d323ae4402be7dad82851bb62956319cc778e1
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2025-09-01 15:00:48 +0300

    AUTH views

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .models import Pet, AdoptionRequest
+from .serializers import (
+    PetSerializer, 
+    SignupSerializer, 
+    UserSerializer, 
+    AdoptionRequestSerializer 
+)
 
-# Create your views here.
+#* AUTH
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key, "username": user.username})
+
